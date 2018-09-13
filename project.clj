b6ec07264817aef110803831293061eb23e4486d
Markovic Vladimir <markovic.vladimir86@gmail.com>
2018-09-13 18:15:28 +0200

    Formatting code.

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
 (defproject org.vladimir/ocr-middle "0.1.0"
-  :description    "Optical character recognition middleware"
-  :url            "http://gitlab:1610/VladimirMarkovic86/ocr-middle"
-  :license        {:name "Eclipse Public License"
-                   :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies   [[org.clojure/clojure	"1.9.0"]
-                   [org.clojure/clojurescript "1.10.339"]
-                   [org.vladimir/common-middle "0.1.0"]
-                   ]
+  :description "Optical character recognition middleware"
+  :url "http://gitlab:1610/VladimirMarkovic86/ocr-middle"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure	"1.9.0"]
+                 [org.clojure/clojurescript "1.10.339"]
+                 [org.vladimir/common-middle "0.1.0"]
+                 ]
   :source-paths ["src/cljc"])
 

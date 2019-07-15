99195622a3c45e1bc98aca760d92215c79a1091b
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2019-07-15 13:53:31 +0200

    Task 88: Removed README.md and LICENSE files from packed jar files.

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject org.clojars.vladimirmarkovic86/ocr-middle "0.3.9"
+(defproject org.clojars.vladimirmarkovic86/ocr-middle "0.3.10"
   :description "Optical character recognition middleware"
   :url "http://github.com/VladimirMarkovic86/ocr-middle"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/clojurescript "1.10.339"]
-                 [org.clojars.vladimirmarkovic86/common-middle "0.2.9"]
+                 [org.clojars.vladimirmarkovic86/common-middle "0.2.10"]
                  ]
 
   :min-lein-version "2.0.0"
